203fd86533a5e4c5eb5422e3fafb0f63d44682d4
DJ <djq2272@gmail.com>
2013-07-01 22:26:23 +0800

    remove proguard result

--- app/src/tk/djcrazy/MyCC98/LoginActivity.java
+++ app/src/tk/djcrazy/MyCC98/LoginActivity.java
@@ -46,7 +46,7 @@ public class LoginActivity extends BaseFragmentActivity implements
 		OnClickListener {
 	private static final String TAG = "MyCC98";
 
-	public static final boolean IS_PROXY_VERSION = false;
+	public static final boolean IS_PROXY_VERSION = true ;
 
 	public static final String USERNAME = "USERNAME";
 	public static final String PASSWORD32 = "PASSWORD32";
